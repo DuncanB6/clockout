5972470761ad72629a5b66441d18dfc1fd9fdf8c
Duncan <duncan@wapta.ca>
2025-10-29 20:28:56 -0600

    picture displays for one second

--- camera.py
+++ camera.py
@@ -4,19 +4,24 @@ import time
 
 def main():
     camera = Picamera2()
-    camera.configure(camera.create_preview_configuration())
-    
+
+    camera.configure(camera.create_still_configuration(main={"format": "RGB888"}))
+
     camera.start()
     time.sleep(2)
     
+    print("Annnnnnnd...")
     image = camera.capture_array()
-    
+    print("CLICK!")
+
+    image = cv2.resize(image, (640, 360))
     cv2.imshow("Captured Image", image)
-    print("Press any key to close the window...")
-    cv2.waitKey(0)
+    cv2.waitKey(2000)
     
     cv2.destroyAllWindows()
     camera.close()
 
 if __name__ == "__main__":
     main()
+
+
